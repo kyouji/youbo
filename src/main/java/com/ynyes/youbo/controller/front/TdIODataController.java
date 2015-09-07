804c149674a31b83870474a9334653c32f9f547a
nieshr <nieshr@qq.com>
2015-09-07 22:10:11 +0800

    新增停车场数据上传功能

---
+++ src/main/java/com/ynyes/youbo/controller/front/TdIODataController.java
@@ -0,0 +1,49 @@
+package com.ynyes.youbo.controller.front;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.ynyes.youbo.entity.TdIOData;
+import com.ynyes.youbo.service.TdDiySiteService;
+import com.ynyes.youbo.service.TdIODataService;
+
+/**
+ * 停车场出入记录
+ *
+ */
+@Controller
+public class TdIODataController {
+
+    @Autowired
+    private TdIODataService tdIODataService;
+    
+    @Autowired
+    private TdDiySiteService tdDiySiteService;
+
+    @RequestMapping(value = "/iodata/save", method=RequestMethod.POST)
+    @ResponseBody
+    public String addCart(TdIOData ioData,
+            HttpServletRequest req) {
+        
+        if (null == tdIODataService.findByBusNoAndIoTime(ioData.getBusNo(), ioData.getIoTime()))
+        { 
+            ioData = tdIODataService.save(ioData);
+            
+            if (ioData.getIoState().equals("正常进入"))
+            {
+                
+            }
+            else if (ioData.getIoState().equals("正常外出"))
+            {
+                
+            }
+        }
+
+        return "ok";
+    }
+}
---
+++ src/main/java/com/ynyes/youbo/service/TdIODataService.java
@@ -0,0 +1,135 @@
+package com.ynyes.youbo.service;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ynyes.youbo.entity.TdIOData;
+import com.ynyes.youbo.repository.TdIODataRepo;
+
+/**
+ * TdIOData 服务类
+ * 
+ * @author Sharon
+ *
+ */
+
+@Service
+@Transactional
+public class TdIODataService {
+    
+    @Autowired
+    TdIODataRepo repository;
+    
+    /**
+     * 删除
+     * 
+     * @param id 菜单项ID
+     */
+    public void delete(Long id)
+    {
+        if (null != id)
+        {
+            repository.delete(id);
+        }
+    }
+    
+    /**
+     * 删除
+     * 
+     * @param e 菜单项
+     */
+    public void delete(TdIOData e)
+    {
+        if (null != e)
+        {
+            repository.delete(e);
+        }
+    }
+    
+    public void delete(List<TdIOData> entities)
+    {
+        if (null != entities)
+        {
+            repository.delete(entities);
+        }
+    }
+    
+    /**
+     * 查找
+     * 
+     * @param id ID
+     * @return
+     */
+    public TdIOData findOne(Long id)
+    {
+        if (null == id)
+        {
+            return null;
+        }
+        
+        return repository.findOne(id);
+    }
+    
+    /**
+     * 查找
+     * 
+     * @param ids
+     * @return
+     */
+    public List<TdIOData> findAll(Iterable<Long> ids)
+    {
+        return (List<TdIOData>) repository.findAll(ids);
+    }
+    
+    public List<TdIOData> findAll()
+    {
+        return (List<TdIOData>) repository.findAll();
+    }
+    
+    public Page<TdIOData> findAllOrderBySortIdAsc(int page, int size)
+    {
+        PageRequest pageRequest = new PageRequest(page, size, new Sort(Direction.ASC, "sortId"));
+        
+        return repository.findAll(pageRequest);
+    }
+    
+    public List<TdIOData> findAllOrderBySortIdAsc()
+    {
+        return (List<TdIOData>) repository.findAll(new Sort(Direction.ASC, "sortId"));
+    }
+    
+    public TdIOData findByBusNoAndIoTime(String busNo, Date ioTime)
+    {
+        if (null == busNo || null == ioTime)
+        {
+            return null;
+        }
+        
+        return repository.findTopByBusNoAndIoTime(busNo, ioTime);
+    }
+    
+    /**
+     * 保存
+     * 
+     * @param e
+     * @return
+     */
+    public TdIOData save(TdIOData e)
+    {
+        return repository.save(e);
+    }
+    
+    public List<TdIOData> save(List<TdIOData> entities)
+    {
+        
+        return (List<TdIOData>) repository.save(entities);
+    }
+}
